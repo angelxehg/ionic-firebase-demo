27ff9b0483a35f02fb662cee93cc8cbbdf061944
Angel Hurtado <angelxehg@pm.me>
2021-11-16 20:21:09 -0600

    Create TabsLayout component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,22 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { TabsLayoutComponent } from './layouts/tabs-layout/tabs-layout.component';
 
 const routes: Routes = [
-  {
-    path: 'home',
-    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
-  },
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
+  },
+  {
+    path: '',
+    component: TabsLayoutComponent,
+    children: [
+      {
+        path: 'home',
+        loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
+      },
+    ]
   },
 ];
 
---
+++ src/app/layouts/tabs-layout/tabs-layout.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tabs-layout',
+  templateUrl: './tabs-layout.component.html',
+  styleUrls: ['./tabs-layout.component.scss'],
+})
+export class TabsLayoutComponent {
+
+  constructor() { }
+
+}
